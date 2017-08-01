346da736fe95bc96ba0fbc0e42010e8341c139ed
Erik on Apollo <ewfalor@gmail.com>
2017-08-01 10:33:43 -0600

    pulsing LED to indicate functional operation

--- Keyboard_Mouse/Keyboard_Mouse.ino
+++ Keyboard_Mouse/Keyboard_Mouse.ino
@@ -3,31 +3,48 @@
 int LED = 13;
 
 void setup() {
-  Mouse.begin();
-  pinMode(LED, OUTPUT);
-  randomSeed(analogRead(0));
-  Serial.begin(9600);
+	Mouse.begin();
+	pinMode(LED, OUTPUT);
+	randomSeed(analogRead(0));
+	Serial.begin(9600);
 }
 
 
 //Sparatic Mouse movements
 void sporatic() {
-  int x = random(10) - 5;
-  int y = random(10) - 5;
-  Mouse.move(x, y);
+#define SPAN     400
+#define HALFSPAN (SPAN / 2)
+	int x = random(SPAN) - HALFSPAN;
+	int y = random(SPAN) - HALFSPAN;
+	Mouse.move(x, y);
 }
 
-//LED blinks just for testing purposes
-void LED_Blink() {
-  digitalWrite(LED, HIGH);
-  delay(200);
-  digitalWrite(LED, LOW);
-  delay(200);
+
+void LED_Pulse() {
+#define MAX 20
+#define MIN  0
+
+	int on  = MIN,
+		off = MAX;
+	for (; on < MAX; on++, off--) {
+		digitalWrite(LED, LOW);
+		delay(off);
+		digitalWrite(LED, HIGH);
+		delay(on);
+	}
+	delay(MAX);
+
+	for (; on > MIN; on--, off++) {
+		digitalWrite(LED, LOW);
+		delay(off);
+		digitalWrite(LED, HIGH);
+		delay(on);
+	}
+	digitalWrite(LED, LOW);
 }
 
 void loop(){
-  for (int i = 0; i < 10; ++i)
-    LED_Blink();
-  sporatic();
+	sporatic();
+	LED_Pulse();
+	delay(5000);
 }
-
---
+++ Keyboard_Mouse/README.md
@@ -0,0 +1,2 @@
+Screensaver preventer - randomly moves the mouse every few seconds so as to
+avoid logouts and lockouts due to user inactivity
