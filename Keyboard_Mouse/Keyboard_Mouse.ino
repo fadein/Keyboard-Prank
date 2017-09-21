f6657934f8f084295113eea8a3ac609d5eff3401
Erik on Apollo <ewfalor@gmail.com>
2017-09-21 08:01:44 -0600

    Refactor code for less obvious operation
    sleep longer, pulse dimmer, move mouse less far

--- Keyboard_Mouse/Keyboard_Mouse.ino
+++ Keyboard_Mouse/Keyboard_Mouse.ino
@@ -1,38 +1,41 @@
 #include <Mouse.h>
 
-int LED = 13;
+#undef DEBUG
+
+#define LED  13
 
 void setup() {
 	Mouse.begin();
 	pinMode(LED, OUTPUT);
 	randomSeed(analogRead(0));
+#ifdef DEBUG
 	Serial.begin(9600);
+#endif
 }
 
 
-//Sparatic Mouse movements
-void sporatic() {
-#define SPAN     400
+#define SPAN     2
 #define HALFSPAN (SPAN / 2)
+//Sporatic Mouse movements
+void sporatic() {
 	int x = random(SPAN) - HALFSPAN;
 	int y = random(SPAN) - HALFSPAN;
 	Mouse.move(x, y);
 }
 
-
-void LED_Pulse() {
 #define MAX 20
+#define MED 10
 #define MIN  0
-
+void LED_Pulse(int maxi) {
 	int on  = MIN,
-		off = MAX;
-	for (; on < MAX; on++, off--) {
+		off = maxi;
+	for (; on < maxi; on++, off--) {
 		digitalWrite(LED, LOW);
 		delay(off);
 		digitalWrite(LED, HIGH);
 		delay(on);
 	}
-	delay(MAX);
+	delay(20);
 
 	for (; on > MIN; on--, off++) {
 		digitalWrite(LED, LOW);
@@ -43,8 +46,13 @@ void LED_Pulse() {
 	digitalWrite(LED, LOW);
 }
 
-void loop(){
-	sporatic();
-	LED_Pulse();
-	delay(5000);
+
+void loop() {
+	int i = 12;
+	for (; i >= 0; --i) {
+		LED_Pulse(MED);
+		delay(1000);
+	}
+	LED_Pulse(MAX);
+	sporatic();	
 }
